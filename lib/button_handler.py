d2f646b5d9cb40791f84ecc7bc9224725c567c8a
Ppamo <pablo@ppamo.cl>
2015-12-12 04:12:31 +0000

    game_by_time: refactor callbacks structure

--- lib/button_handler.py
+++ lib/button_handler.py
@@ -9,6 +9,7 @@ class button_handler(object):
 		self.a_pin = 18
 		self.b_pin = 23
 		self.c_pin = 24
+		self.button_channel_map = { 'a': self.a_pin, 'b': self.b_pin, 'c': self.c_pin }
 		GPIO.setmode(GPIO.BCM)
 		GPIO.setup(self.a_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 		GPIO.setup(self.b_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -16,14 +17,8 @@ class button_handler(object):
 		GPIO.add_event_detect(self.a_pin, GPIO.BOTH, callback=self.button_callback)
 		GPIO.add_event_detect(self.b_pin, GPIO.BOTH, callback=self.button_callback)
 		GPIO.add_event_detect(self.c_pin, GPIO.BOTH, callback=self.button_callback)
-		self.a_click_callbacks = []
-		self.a_hold_callbacks = []
-		self.b_click_callbacks = []
-		self.b_hold_callbacks = []
-		self.c_click_callbacks = []
-		self.c_hold_callbacks = []
-		self.click_callbacks_by_channel = { self.a_pin: self.a_click_callbacks, self.b_pin: self.b_click_callbacks, self.c_pin: self.c_click_callbacks }
-		self.hold_callbacks_by_channel = { self.a_pin: self.a_hold_callbacks, self.b_pin: self.b_hold_callbacks, self.c_pin: self.c_hold_callbacks }
+		dummy_callback = { 'click': [], 'hold':[], 'double':[] }
+		self.callbacks = { self.a_pin: dummy_callback, self.b_pin: dummy_callback, self.c_pin: dummy_callback }
 		self.click_event_timer = {}
 		self.event_delta = datetime.timedelta(milliseconds = 500)
 		self.event_hold_delta = datetime.timedelta(milliseconds = 1000)
@@ -31,8 +26,8 @@ class button_handler(object):
 		self.last_pressed_event = {18: dummy_event, 23: dummy_event, 24: dummy_event}
 		self.last_triggered_event = {18: dummy_event, 23: dummy_event, 24: dummy_event}
 
-	def execute_callbacks(self, callbacks):
-		for callback in callbacks:
+	def execute_callbacks(self, channel, event):
+		for callback in self.callbacks[channel][event]:
 			if callback is not None:
 				callback()
 
@@ -65,7 +60,7 @@ class button_handler(object):
 		if delta > self.event_delta:
 			if hold_delta > self.event_hold_delta:
 				self.last_triggered_event.update({channel: { 'time': now, 'status': 1}})
-				self.execute_callbacks(self.hold_callbacks_by_channel[channel])
+				self.execute_callbacks(channel, 'hold')
 				print "events: triggered hold event"
 			else:
 				if channel in self.click_event_timer:
@@ -73,7 +68,7 @@ class button_handler(object):
 					self.click_event_timer[channel].cancel()
 					del self.click_event_timer[channel]
 					self.last_triggered_event.update({channel: { 'time': now, 'status': 2}})
-					self.execute_callbacks(self.hold_callbacks_by_channel[channel])
+					self.execute_callbacks(channel, 'double')
 					print "events: triggered double click event at ".format(datetime.datetime.now())
 				else:
 					print "events: enqueued click event at {}".format(datetime.datetime.now())
@@ -83,27 +78,17 @@ class button_handler(object):
 	def click_trigger(self, channel):
 		now = datetime.datetime.now()
 		self.last_triggered_event.update({channel: { 'time': now, 'status': 0}})
-		self.execute_callbacks(self.click_callbacks_by_channel[channel])
+		self.execute_callbacks(channel, 'click')
 		del self.click_event_timer[channel]
 		print "events: triggered click event at {}".format(datetime.datetime.now())
 
 	def add_handler(self, button, handler = None, event = 'click'):
-		if button == 'a':
-			list = self.a_click_callbacks if event == 'click' else self.a_hold_callbacks
-		elif button == 'b':
-			list = self.b_click_callbacks if event == 'click' else self.b_hold_callbacks
-		elif button == 'c':
-			list = self.c_click_callbacks if event == 'click' else self.c_hold_callbacks
+		list = self.callbacks[self.button_channel_map[button]][event]
 		list.append(handler);
 		return len(list) - 1
 	
 	def remove_handler(self, button, index, event = 'click'):
-		if button == 'a':
-			list = self.a_click_callbacks if event == 'click' else self.a_hold_callbacks
-		elif button == 'b':
-			list = self.b_click_callbacks if event == 'click' else self.b_hold_callbacks
-		elif button == 'c':
-			list = self.c_click_callbacks if event == 'click' else self.c_hold_callbacks
+		list = self.callbacks[self.button_channel_map[button]][event]
 		list[index] = None
 
 	def terminate(self):
